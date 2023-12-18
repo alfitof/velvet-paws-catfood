e4c8ec10e05daab4de38f6343c60b1becac692ac
alfitof <alfito.fbriansyah@gmail.com>
2023-12-18 20:53:08 +0700

    newest

--- store/cartStore.js
+++ store/cartStore.js
@@ -1,32 +1,21 @@
 // stores/cartStore.js
 import create from "zustand";
 
-const useCartStore = create((set) => {
-  // const isBrowser = typeof window !== "undefined";
+const useCartStore = create((set) => ({
+  cartItems: [],
 
-  const initialState = {
-    cartItems: [],
-  };
-
-  set(initialState);
-
-  return {
-    ...initialState,
-
-    addToCart: (item) => {
-      set((state) => {
-        const updatedCartItems = [...state.cartItems, item];
-        return { cartItems: updatedCartItems };
-      });
-    },
-
-    removeFromCart: (index) =>
-      set((state) => {
-        const updatedCart = [...state.cartItems];
-        updatedCart.splice(index, 1);
-        return { cartItems: updatedCart };
-      }),
-  };
-});
+  addToCart: (item) => {
+    set((state) => {
+      const updatedCartItems = [...state.cartItems, item];
+      return { cartItems: updatedCartItems };
+    });
+  },
+  removeFromCart: (index) =>
+    set((state) => {
+      const updatedCart = [...state.cartItems];
+      updatedCart.splice(index, 1);
+      return { cartItems: updatedCart };
+    }),
+}));
 
 export default useCartStore;
